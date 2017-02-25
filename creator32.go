fec5bf4480520e42903f7a05cc99e50c094fe341
Alex Nichol <unixpickle@gmail.com>
2017-02-24 20:52:08 -0500

    added non-kernelized built-ins

--- creator32.go
+++ creator32.go
@@ -1,6 +1,9 @@
 package cudavec
 
-import "github.com/unixpickle/anyvec"
+import (
+	"github.com/unixpickle/anyvec"
+	"github.com/unixpickle/cuda"
+)
 
 // A Creator32 is an anyvec.Creator for vectors using
 // float32 numerics and []float32 slice types.
@@ -24,21 +27,81 @@ func (c *Creator32) MakeNumericList(x []float64) anyvec.NumericList {
 
 // MakeVector creates a zero'd out anyvec.Vector.
 func (c *Creator32) MakeVector(size int) anyvec.Vector {
-	panic("nyi")
+	return &vector32{
+		creator: c,
+		size:    size,
+	}
 }
 
 // MakeVectorData creates an anyvec.Vector with the
 // specified contents.
-func (c *Creator32) MakeVectorData(dObj anyvec.NumericList) anyvec.Vector {
-	panic("nyi")
+func (c *Creator32) MakeVectorData(list anyvec.NumericList) anyvec.Vector {
+	slice := list.([]float32)
+	res := c.MakeVector(len(slice))
+	res.SetData(slice)
+	return res
 }
 
 // Concat concatenates vectors.
 func (c *Creator32) Concat(v ...anyvec.Vector) anyvec.Vector {
-	panic("nyi")
+	totalLen := 0
+	for _, x := range v {
+		// Type assertion to ensure we panic during the call if
+		// the type is bad.
+		totalLen += x.(*vector32).Len()
+
+		// Integer overflow.
+		if totalLen < 0 {
+			panic("concatenated size is too long")
+		}
+	}
+
+	res := &vector32{
+		creator: c,
+		size:    totalLen,
+	}
+
+	c.run(func() error {
+		buf, err := cuda.AllocBuffer(c.Handle.allocator, uintptr(totalLen)*4)
+		if err != nil {
+			return err
+		}
+		var off uintptr
+		for _, x := range v {
+			subSlice := cuda.Slice(buf, off, off+uintptr(x.Len()))
+			rawX := x.(*vector32)
+			if rawX.buffer != nil {
+				if err := cuda.CopyBuffer(subSlice, rawX.buffer); err != nil {
+					return err
+				}
+			} else {
+				if err := cuda.ClearBuffer(subSlice); err != nil {
+					return err
+				}
+			}
+			off += uintptr(x.Len()) * 4
+		}
+		res.buffer = buf
+		return nil
+	})
+
+	return res
 }
 
 // MakeMapper creates a mapper.
 func (c *Creator32) MakeMapper(inSize int, table []int) anyvec.Mapper {
 	panic("nyi")
+}
+
+func (c *Creator32) run(f func() error) <-chan error {
+	return c.Handle.context.Run(func() error {
+		if err := f(); err != nil {
+			panic(err)
+		}
+		return nil
+	})
+}
+
+func (c *Creator32) runSync(f func() error) {
+	<-c.run(f)
 }
--- vector32.go
+++ vector32.go
@@ -3,6 +3,7 @@ package cudavec
 import (
 	"github.com/unixpickle/anyvec"
 	"github.com/unixpickle/cuda"
+	"github.com/unixpickle/cuda/cublas"
 )
 
 type vector32 struct {
@@ -35,7 +36,7 @@ func (v *vector32) Data() anyvec.NumericList {
 func (v *vector32) SetData(d anyvec.NumericList) {
 	slice := d.([]float32)
 	if len(slice) > v.Len() {
-		panic("index out of bounds")
+		panic("index out of range")
 	}
 	v.runSync(func() error {
 		if err := v.lazyInit(len(slice) < v.Len()); err != nil {
@@ -45,26 +46,184 @@ func (v *vector32) SetData(d anyvec.NumericList) {
 	})
 }
 
+func (v *vector32) Set(v1 anyvec.Vector) {
+	v.assertCompat(v1, false)
+	v.run(func() error {
+		buf1 := v1.(*vector32).buffer
+		if buf1 == nil {
+			if v.buffer != nil {
+				return cuda.ClearBuffer(v.buffer)
+			}
+			return nil
+		}
+		if err := v.lazyInit(false); err != nil {
+			return err
+		}
+		return cuda.CopyBuffer(v.buffer, buf1)
+	})
+}
+
+func (v *vector32) Copy() anyvec.Vector {
+	v1 := v.Creator().MakeVector(v.Len())
+	v1.Set(v)
+	return v1
+}
+
+func (v *vector32) Slice(start, end int) anyvec.Vector {
+	if start < 0 || start > end || end > v.Len() {
+		panic("index out of range")
+	}
+	res := &vector32{creator: v.creator, size: end - start}
+	v.run(func() (err error) {
+		if v.buffer == nil {
+			return nil
+		}
+		subSlice := cuda.Slice(v.buffer, uintptr(start)*4, uintptr(end)*4)
+		res.buffer, err = cuda.AllocBuffer(v.creator.Handle.allocator,
+			uintptr(res.size)*4)
+		if err != nil {
+			return err
+		}
+		return cuda.CopyBuffer(res.buffer, subSlice)
+	})
+	return res
+}
+
+func (v *vector32) SetSlice(start int, v1 anyvec.Vector) {
+	src := v1.(*vector32)
+	if src.Len() > v.Len()-start {
+		panic("index out of range")
+	} else if start <= -src.Len() {
+		return
+	}
+
+	v.run(func() error {
+		if src.buffer == nil && v.buffer == nil {
+			return nil
+		}
+		dstStart := start
+		srcStart := 0
+		if start < 0 {
+			dstStart = 0
+			srcStart = -start
+		}
+		copyCount := src.Len() - srcStart
+		if v.Len()-dstStart < copyCount {
+			copyCount = v.Len() - dstStart
+		}
+		if err := v.lazyInit(true); err != nil {
+			return err
+		}
+		dst := cuda.Slice(v.buffer, uintptr(dstStart)*4,
+			uintptr(dstStart+copyCount)*4)
+		if src.buffer == nil {
+			return cuda.ClearBuffer(dst)
+		}
+		srcSlice := cuda.Slice(src.buffer, uintptr(srcStart)*4,
+			uintptr(srcStart+copyCount)*4)
+		return cuda.CopyBuffer(dst, srcSlice)
+	})
+}
+
 func (v *vector32) Scale(s anyvec.Numeric) {
+	scaler := s.(float32)
 	v.run(func() error {
 		if v.buffer == nil {
 			return nil
 		}
-		return v.creator.Handle.blas.Sscal(v.Len(), s.(float32), v.buffer, 1)
+		return v.creator.Handle.blas.Sscal(v.Len(), scaler, v.buffer, 1)
+	})
+}
+
+func (v *vector32) AddScaler(s anyvec.Numeric) {
+	panic("nyi")
+}
+
+func (v *vector32) Dot(v1 anyvec.Vector) anyvec.Numeric {
+	v.assertCompat(v1, true)
+	var res float32
+	v.runSync(func() error {
+		v32 := v1.(*vector32)
+		if v32.buffer == nil || v.buffer == nil {
+			return nil
+		}
+		return v.creator.Handle.blas.Sdot(v.Len(), v.buffer, 1, v32.buffer, 1, &res)
+	})
+	return res
+}
+
+func (v *vector32) Add(v1 anyvec.Vector) {
+	v.axpy(1, v1)
+}
+
+func (v *vector32) Sub(v1 anyvec.Vector) {
+	v.axpy(-1, v1)
+}
+
+func (v *vector32) Mul(v1 anyvec.Vector) {
+	v.assertCompat(v1, false)
+	panic("nyi")
+}
+
+func (v *vector32) Div(v1 anyvec.Vector) {
+	v.assertCompat(v1, false)
+	panic("nyi")
+}
+
+func (v *vector32) Gemm(transA, transB bool, m, n, k int,
+	alpha anyvec.Numeric, a anyvec.Vector, lda int,
+	b anyvec.Vector, ldb int, beta anyvec.Numeric, ldc int) {
+	alphaFloat := alpha.(float32)
+	betaFloat := beta.(float32)
+	a32 := a.(*vector32)
+	b32 := b.(*vector32)
+	v.run(func() error {
+		v.lazyInit(true)
+		a32.lazyInit(true)
+		b32.lazyInit(true)
+		ta := cublas.NoTrans
+		tb := cublas.NoTrans
+		if transA {
+			ta = cublas.Trans
+		}
+		if transB {
+			tb = cublas.Trans
+		}
+		return v.creator.Handle.blas.Sgemm(ta, tb, m, n, k,
+			alphaFloat, a32.buffer, lda, b32.buffer, ldb,
+			betaFloat, v.buffer, ldc)
+	})
+}
+
+func (v *vector32) axpy(scaler float32, v1 anyvec.Vector) {
+	v.assertCompat(v1, false)
+	v.run(func() error {
+		v32 := v1.(*vector32)
+		if v32.buffer == nil {
+			return nil
+		} else if v.buffer == nil {
+			if err := v.lazyInit(false); err != nil {
+				return err
+			}
+			if err := cuda.CopyBuffer(v.buffer, v32.buffer); err != nil {
+				return err
+			}
+			if scaler == 1 {
+				return nil
+			}
+			return v.creator.Handle.blas.Sscal(v.Len(), scaler, v.buffer, 1)
+		}
+		return v.creator.Handle.blas.Saxpy(v.Len(), scaler, v32.buffer, 1,
+			v.buffer, 1)
 	})
 }
 
 func (v *vector32) run(f func() error) <-chan error {
-	return v.creator.Handle.context.Run(func() error {
-		if err := f(); err != nil {
-			panic(err)
-		}
-		return nil
-	})
+	return v.creator.run(f)
 }
 
 func (v *vector32) runSync(f func() error) {
-	<-v.run(f)
+	v.creator.runSync(f)
 }
 
 func (v *vector32) lazyInit(clear bool) error {
@@ -80,4 +239,13 @@ func (v *vector32) lazyInit(clear bool) error {
 		return cuda.ClearBuffer(v.buffer)
 	}
 	return nil
+}
+
+func (v *vector32) assertCompat(v1 anyvec.Vector, readOnly bool) {
+	v132 := v1.(*vector32)
+	if readOnly || v == v132 {
+		panic("vectors cannot be equal")
+	} else if v.Len() != v132.Len() {
+		panic("length mismatch")
+	}
 }
